052dcc8bba146ca068b8586ce38aafc2ec731ed9
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2018-11-27 00:17:12 -0500

    add Monitor process to notify that the process had finised already to the Server

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -2,17 +2,24 @@ defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
   setup do
-    registy = start_supervised!(KV.Registry)
-    %{registy: registy}
+    registry = start_supervised!(KV.Registry)
+    %{registry: registry}
   end
 
-  test "spawn buckets", %{registy: registy} do
-    assert KV.Registry.lookup(registy, "shopping") == :error
+  test "spawn buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
 
-    KV.Registry.create(registy, "shopping")
-    assert {:ok, bucket} = KV.Registry.lookup(registy, "shopping")
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
