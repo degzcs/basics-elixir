9ad006789984887e6cad4dc12b734302530790cf
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2018-11-27 23:36:47 -0500

    add test to verify after a bucket crashed the bucket is not running anymore for sure

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -2,7 +2,7 @@ defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
   setup context do
-    #registry = start_supervised!(KV.Registry)
+    # registry = start_supervised!(KV.Registry)
     _ = start_supervised!({KV.Registry, name: context.test})
     %{registry: context.test}
   end
@@ -37,5 +37,16 @@ defmodule KV.RegistryTest do
     # Do a call to ensure the registry processed the DOWN message
     _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Simulate a bucket crash by explicitly and synchronously shutting it down.
+    Agent.stop(bucket, :shutdown)
+
+    # Now trying to call the dead process causes a :noproc exit
+    catch_exit(KV.Bucket.put(bucket, "milk", 3))
   end
 end
